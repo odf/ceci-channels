c9ab19d236cbe4060eb219cc1bdf3fcceecf322f
odf <olaf.delgado@gmail.com>
2013-12-31 13:27:50 +1100

    refactor lossy.js in examples

--- examples/lossy.js
+++ examples/lossy.js
@@ -3,32 +3,37 @@
 var core = require('ceci-core');
 var cc   = require('../index');
 
-var run = function(buffer) {
-  var ch = cc.chan(buffer);
-
+var writeThings = function(ch) {
   core.go(function*() {
     for (var i = 1; ; ++i)
       if (!(yield cc.push(ch, i)))
         break;
   });
+};
 
+var readThings = function(ch) {
   return core.go(function*() {
+    var a = [];
     var i;
-    for (i = 0; i < 20; ++i) {
-      yield cc.pull(cc.timeout(0));
-      console.log(yield cc.pull(ch));
+    for (i = 0; i < 10; ++i) {
+      yield cc.pull(cc.timeout(1));
+      a.push(yield cc.pull(ch));
     }
     cc.close(ch);
-    return;
+    return a;
   });
 };
 
+var run = function(buffer) {
+  var ch = cc.chan(buffer);
+
+  writeThings(ch);
+  return readThings(ch);
+};
+
 core.go(function*() {
-  yield run(new cc.Buffer(0));
-  console.log();
-  yield run(new cc.Buffer(5));
-  console.log();
-  yield run(new cc.DroppingBuffer(5));
-  console.log();
-  run(new cc.SlidingBuffer(5));
+  console.log(yield run());
+  console.log(yield run(new cc.Buffer(5)));
+  console.log(yield run(new cc.DroppingBuffer(5)));
+  console.log(yield run(new cc.SlidingBuffer(5)));
 });
