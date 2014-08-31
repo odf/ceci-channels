447a64470b395a3a66029b05284a25d0cd99ca19
odf <olaf.delgado@gmail.com>
2014-08-31 20:20:24 +1000

    add (untested) channel model for testing

--- spec/channels_spec.js
+++ spec/channels_spec.js
@@ -2,24 +2,141 @@
 
 require('comfychair/jasmine');
 var comfy = require('comfychair');
+var chan = require('../index');
 
-describe('a simple predicate testing for positivity', function() {
-  var pred = function(n) {
-    return (n > 0) ? comfy.success() : comfy.failure('must be positive');
+
+var merge = function() {
+  var args = Array.prototype.slice.call(arguments);
+  var result = args.every(Array.isArray) ? [] : {};
+  var i, obj, key;
+  for (i in args) {
+    obj = args[i];
+    for (key in obj)
+      result[key] = obj[key];
+  }
+  return result;
+};
+
+
+function model(type) {
+  var _transitions = {
+    init: function(state, arg) {
+      return {
+        state: {
+          count  : 0,
+          buffer : [],
+          bsize  : arg,
+          pullers: [],
+          pushers: [],
+          closed : false
+        }
+      };
+    },
+    push: function(state, val) {
+      var h = state.count + 1;
+      state = state.merge({ count: h });
+
+      if (state.closed) {
+        return {
+          state : state,
+          output: [[h, false]]
+        };
+      } else if (state.pullers.length > 0) {
+        return {
+          state : merge(state, { pullers: state.pullers.slice(1) }),
+          output: [[state.pullers[0], val], [h, true]]
+        };
+      } else if (state.bsize > state.buffer.length) {
+        return {
+          state : merge(state, { buffer: state.buffer.concat([val]) }),
+          output: [[h, true]]
+        };
+      } else {
+        return {
+          state : merge(state, { pushers: state.pushers.concat([[h, val]]) }),
+          output: []
+        };
+      }
+    },
+    pull: function(state) {
+      var h = state.count + 1;
+      state = state.merge({ count: h });
+
+      if (state.buffer.length > 0) {
+        if (state.pushers.length > 0) {
+          return {
+            state: state.merge({
+              buffer : state.buffer.slice(1).concat(state.pushers[0][1]),
+              pushers: state.pushers.slice(1)
+            }),
+            output: [[state.pushers[0][0], true], [h, state.buffer[0]]]
+          };
+        } else {
+          return {
+            state: state.merge({
+              buffer: state.buffer.slice(1)
+            }),
+            output: [[h, state.buffer[0]]]
+          };
+        }
+      } else if (state.closed) {
+        return {
+          state : state,
+          output: [[h, undefined]]
+        };
+      } else if (state.pushers.length > 0) {
+        return {
+          state : merge(state, { pushers: state.pushers.slice(1) }),
+          output: [[state.pushers[0][0], true], [h, state.pushers[0][1]]]
+        };
+      } else {
+        return {
+          state : merge(state, { pullers: state.pullers.concat([h]) }),
+          output: []
+        };
+      }
+    },
+    close: function(state) {
+      return {
+        state: state.merge({
+          buffer : [],
+          pullers: [],
+          pushers: [],
+          closed : true
+        }),
+        output: [].concat(
+          state.pushers.map(function(p) { return [p[0], false]; }),
+          state.pullers.map(function(p) { return [p, undefined]; }))
+      };
+    }
   };
 
-  describe('applied a natural number generator', function() {
-    var gen = function(n) {
-      return comfy.randomInt(1, Math.max(1, n));
-    };
+  var _hasArgument = function(command) {
+    return ['init', 'push'].indexOf(command) >= 0;
+  };
 
-    var shrink = function(n) {
-      return (n > 1) ? [n-1] : [];
-    };
+  return {
+    commands: function() {
+      var cmds = Object.keys(this._transitions).slice();
+      cmds.splice(cmds.indexOf('init'), 1);
+      return cmds;
+    },
+    randomArgs: function(command, size) {
+      if (this._hasArgument(command))
+        return [comfy.randomInt(0, size)];
+      else
+        return [];
+    },
 
-    it('succeeds', function() {
-      expect(pred).toSucceedOn(gen, shrink);
-      expect(pred).toSucceedOn(gen);
-    });
-  });
-});
+    shrinkArgs: function(command, args) {
+      if (this._hasArgument(command) && args[0] > 0)
+        return [[args[0] - 1]];
+      else
+        return [];
+    },
+
+    apply: function(state, command, args) {
+      return this._transitions[command].apply(null, [state].concat(args));
+    }
+  };
+};
